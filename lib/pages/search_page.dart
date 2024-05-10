2f0e8c552392a3b960159ad4416f61a92a38b171
Khusanboy Tursunov <tursunovkhusanboy@gmail.com>
2024-05-10 06:02:37 +0500

    added search, popular and recommendation for you sections

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -5,14 +6,404 @@ class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFF05001E),
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              height: 50.h,
-            )
-          ],
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 15.h),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: double.infinity,
+                height: 45.h,
+                child: TextField(
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.sp,
+                      fontWeight: FontWeight.w400,
+                      letterSpacing: 0.5),
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.all(20),
+                    filled: true,
+                    fillColor: const Color(0xFF37324B),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none,
+                    ),
+                    hintText: "Search by title, genre, actor",
+                    hintStyle: TextStyle(
+                      color: const Color(0xFFAAAAB9),
+                      fontSize: 16.sp,
+                      letterSpacing: 0.5,
+                      fontWeight: FontWeight.w400,
+                      decorationThickness: 6,
+                    ),
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.only(left: 25.w, right: 15.w),
+                      child: const Icon(
+                        CupertinoIcons.search,
+                        size: 30,
+                      ),
+                    ),
+                    prefixIconColor: const Color(0xFFAAAAB9),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 25.h,
+              ),
+              const Text(
+                "Recent Searches",
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 18,
+                ),
+              ),
+              SizedBox(
+                height: 10.h,
+              ),
+              const Wrap(
+                spacing: 10,
+                runSpacing: 5,
+                children: [
+                  Chip(
+                    label: Text(
+                      "Marvel",
+                      style: TextStyle(
+                        color: Colors.blue,
+                      ),
+                    ),
+                    avatar: Icon(
+                      CupertinoIcons.clock,
+                      color: Colors.blue,
+                    ),
+                    backgroundColor: Color(0xFF0F0A32),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                    ),
+                  ),
+                  Chip(
+                    label: Text(
+                      "Captain america",
+                      style: TextStyle(
+                        color: Colors.blue,
+                      ),
+                    ),
+                    avatar: Icon(
+                      CupertinoIcons.clock,
+                      color: Colors.blue,
+                    ),
+                    backgroundColor: Color(0xFF0F0A32),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                    ),
+                  ),
+                  Chip(
+                    label: Text(
+                      "Captain Marvel",
+                      style: TextStyle(
+                        color: Colors.blue,
+                      ),
+                    ),
+                    avatar: Icon(
+                      CupertinoIcons.clock,
+                      color: Colors.blue,
+                    ),
+                    backgroundColor: Color(0xFF0F0A32),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                    ),
+                  ),
+                  Chip(
+                    label: Text(
+                      "Thor",
+                      style: TextStyle(
+                        color: Colors.blue,
+                      ),
+                    ),
+                    avatar: Icon(
+                      CupertinoIcons.clock,
+                      color: Colors.blue,
+                    ),
+                    backgroundColor: Color(0xFF0F0A32),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              const Text(
+                "Popular",
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 18,
+                ),
+              ),
+              SizedBox(
+                height: 15.h,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Wrap(
+                  spacing: 10.w,
+                  children: [
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/images/ladybug.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/images/ladybug.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/images/ladybug.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/images/ladybug.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/images/ladybug.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/images/ladybug.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/images/ladybug.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/images/ladybug.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/images/ladybug.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/images/ladybug.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              const Text(
+                "Recommendation for you",
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 18,
+                ),
+              ),
+              SizedBox(
+                height: 15.h,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Wrap(
+                  spacing: 10.w,
+                  children: [
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/images/ladybug.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/images/ladybug.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/images/ladybug.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/images/ladybug.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/images/ladybug.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/images/ladybug.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/images/ladybug.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/images/ladybug.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/images/ladybug.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.w,
+                      height: 140.w,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/images/ladybug.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
